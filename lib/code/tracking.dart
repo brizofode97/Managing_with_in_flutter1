7f4e2fae7adeef857df1e04e85230c953373ed03
brizofode97 <brizofode97@gmail.com>
2023-01-05 18:16:15 +0100

    dernier parti updating...

--- lib/code/tracking.dart
+++ lib/code/tracking.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 const List<String> urls = [
@@ -17,24 +15,80 @@ class App extends StatefulWidget {
 }
 
 class AppState extends State<App> {
+  bool isTagging = false;
+  List<PhotoState> photoStates = List.of(urls.map((url) => PhotoState(url)));
+  Set<String> tags = {"all", "nature", "cat"};
+
+  void selectTag(String tag) {
+    setState(() {
+      if (isTagging) {
+        if (tag != "all") {
+          photoStates.forEach((element) {
+            if (element.selected) {
+              element.tags.add(tag);
+            }
+          });
+        }
+      } else {
+        photoStates.forEach((element) {
+          element.display = tag == "all" ? true : element.tags.contains(tag);
+        });
+      }
+    });
+  }
+
+  void toogleTagging(String url) {
+    setState(() {
+      isTagging = !isTagging;
+      photoStates.forEach((element) {
+        if (isTagging && element.url == url) {
+          element.selected = true;
+        } else {
+          element.selected = false;
+        }
+      });
+    });
+  }
+
+  void onPhotoSelect(String url, bool selected) {
+    setState(() {
+      photoStates.forEach((element) {
+        if (element.url == url) {
+          element.selected = selected;
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-        title: 'Photo Viewer', home: GalleryPage("Image Gallery", urls));
+    return MaterialApp(
+        title: 'Photo Viewer',
+        home: GalleryPage('Image Gallery', photoStates, isTagging,
+            toogleTagging, onPhotoSelect));
   }
 }
 
 class PhotoState {
   String url;
-  bool isSelected;
+  bool selected;
+  bool display;
+  Set<String> tags = {};
 
-  PhotoState(this.url, {this.isSelected = false});
+  PhotoState(this.url, {this.selected = false, this.display = true, tags});
 }
 
 class GalleryPage extends StatelessWidget {
   final String title;
-  final List<String> urls;
-  const GalleryPage(this.title, this.urls, {super.key});
+  final List<PhotoState> photoStates;
+  final bool tagging;
+
+  final Function toogleTagging;
+  final Function onPhotoSelect;
+
+  const GalleryPage(this.title, this.photoStates, this.tagging,
+      this.toogleTagging, this.onPhotoSelect,
+      {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -43,17 +97,45 @@ class GalleryPage extends StatelessWidget {
         body: GridView.count(
             primary: false,
             crossAxisCount: 2,
-            children: List.of(urls.map((url) => Photo(url)))));
+            children: List.of(photoStates.map(
+                (ps) => Photo(ps, tagging, toogleTagging, onPhotoSelect)))));
   }
 }
 
 class Photo extends StatelessWidget {
-  final String url;
-  const Photo(this.url, {super.key});
+  final PhotoState state;
+  final bool selectable;
+
+  final Function onLongPress;
+  final Function onSelect;
+
+  const Photo(this.state, this.selectable, this.onLongPress, this.onSelect,
+      {super.key});
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> children = [
+      GestureDetector(
+          child: Image.network(state.url),
+          onLongPress: () => onLongPress(state.url)),
+    ];
+    if (selectable) {
+      children.add(Positioned(
+          left: 20,
+          top: 0,
+          child: Theme(
+              data: Theme.of(context)
+                  .copyWith(unselectedWidgetColor: Colors.grey[200]),
+              child: Checkbox(
+                  onChanged: (value) {
+                    onSelect(state.url, value);
+                  },
+                  value: state.selected,
+                  activeColor: Colors.white,
+                  checkColor: Colors.black))));
+    }
     return Container(
-        padding: const EdgeInsets.only(top: 10), child: Image.network(url));
+        padding: EdgeInsets.only(top: 10),
+        child: Stack(alignment: Alignment.center, children: children));
   }
 }
